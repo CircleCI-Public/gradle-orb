b18d665e9236d441ac182647eb3160b9b7d18879
Ryan Bourdais <67397011+ryanbourdais@users.noreply.github.com>
2023-03-06 09:23:22 -0600

    feat: added arrays to cache checksum (#25)
    * feat: added arrays to cache checksum

--- src/scripts/checksum_files.sh
+++ src/scripts/checksum_files.sh
@@ -1,1 +1,24 @@
-find . -name "${PARAM_CHECKSUM_FILES}" | sort | xargs cat | shasum | awk '{print $1}' > "${CHECKSUM_SEED_LOCATION}"
+#check for an array
+if [[ "${PARAM_CHECKSUM_FILES}" == *","* ]]; then
+    CHECKSUM_STRING=""
+
+    #use IFS to create array delimited by commas
+    IFS=", " read -a CHECKSUM_ARRAY -r <<< "${PARAM_CHECKSUM_FILES}"
+
+    length=${#CHECKSUM_ARRAY[@]}
+
+    #Iterate through the array to add `-or -name "${CHECKSUM_ARRAY[index]}"` for all indexes > 0
+    for (( i=0; i<length; i++ ))
+        do
+            currentString="${CHECKSUM_ARRAY[i]//,/}"
+            if [[ $i == 0 ]]; then
+            CHECKSUM_STRING=$currentString
+            else
+            CHECKSUM_STRING=$CHECKSUM_STRING" -or -name "$currentString
+            fi
+        done
+else 
+    CHECKSUM_STRING="${PARAM_CHECKSUM_FILES}"
+fi
+
+find . -name "${CHECKSUM_STRING}" | sort | xargs cat | shasum | awk '{print $1}' > "${CHECKSUM_SEED_LOCATION}"
